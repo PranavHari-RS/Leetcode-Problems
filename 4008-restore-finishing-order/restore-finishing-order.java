0afaf6264a861c4c7b8e78831e8149705e3fe661
PranavHari-RS <pranavhari29706@gmail.com>
2026-09-11 09:07:40 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (9.58%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -6,7 +6,7 @@ class Solution {
             for(int j=0;j<friends.length;j++){
                 if(order[i]==friends[j]){
                     ans[k++]=order[i];
-                          
+                    break;
                 }
             }
         }
