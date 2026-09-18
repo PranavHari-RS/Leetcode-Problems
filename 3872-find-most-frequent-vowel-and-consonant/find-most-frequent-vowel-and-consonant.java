d0b90dbbee6cfcea18ffae61fa4e3acb8b89500e
PranavHari-RS <pranavhari29706@gmail.com>
2026-09-18 12:17:44 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (65.57%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        int maxV=0;
+        int maxC=0;
+        for(int i=0;i<26;i++){
+            char c = (char)(i+'a');
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+                maxV = Math.max(maxV,freq[i]);
+            }
+            else{
+                maxC = Math.max(maxC,freq[i]);
+            }
+        }
+        return maxV+maxC;
+    }
+}
